3593403161afdf1f5dd6ca6e2e49fdf732f52f40
im-ankitjais <ankitmyself2017@gmail.com>
2021-04-05 11:33:27 +0530

    contents changes

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -10,7 +10,7 @@ import { useQuery, gql } from "@apollo/client";
 import Loader from './Loader'
 const FORGED_ARTICLES = gql`
 {
-  posts{
+  posts(orderBy: id_DESC){
     id
     slug
     excerpt
@@ -44,22 +44,22 @@ function Articles() {
           </div>
             <div className="col-12 col-md-9 p-0 m-0">
                 <TopArticle item={data.posts[0]} />
-                <ArticleBox items={data.posts.slice(1,5)} />
+                <ArticleBox items={data.posts.slice(1,4)} />
                 <div className="item text-right">
-            <Link to="/news/news"><button className="bottom_button">All News
+            {/* <Link to="/news/news"><button className="bottom_button">All News
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-right" className="svgBut" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z"></path></svg>
                 
-              </button></Link>
+              </button></Link> */}
           </div> 
             </div>
             <div className="col-12 col-md-3"></div>
-            {data.posts.slice(2).length > 0 &&
+            {data.posts.slice(4).length > 0 &&
             <div className="main_content">
           <div className="collection_section">
           <div className="top_head">
             <h1>MORE NEWS</h1>
             </div>
-          <DashboardNewsItem moreItems={data.posts.slice(2)}  /> 
+          <DashboardNewsItem moreItems={data.posts.slice(4)}  /> 
           </div>
         </div>
         }
